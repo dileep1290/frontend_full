e667fb4d8473f13f1034ea87bed7236d4d562e77
dileep540 <bdileepkumar663@gmail.com>
2025-01-02 23:25:44 +0530

    commiting some new changes.

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ document
     const userData = { name, email, password };
 
     try {
-      const response = await fetch("http://localhost:8080/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
+      const response = await fetch(
+        "https://glitch.com/edit/#!/tree-substantial-lighter?path=database.js%3A1%3A0",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userData),
+        }
+      );
 
       const data = await response.json();
       if (response.ok) {
